16bfecb89f61d312abbf359186ebdd4e9ccec6a5
karussellbremser <kicker789@hotmail.de>
2021-12-17 03:09:23 +0100

    added some movie properties

---
+++ movie.py
@@ -0,0 +1,7 @@
+class Movie:
+    
+    def __init__(self, id_imdb, name, year, seriestype):
+        self.id_imdb = id_imdb
+        self.name = name
+        self.year = year
+        self.seriestype = seriestype
