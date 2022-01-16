277e4c38bc32598e893eee9bcdbb508728f64e58
karussellbremser <kicker789@hotmail.de>
2022-01-16 05:39:36 +0100

    added mediaversion class

---
+++ mediaversion.py
@@ -0,0 +1,7 @@
+class MediaVersion:
+    
+    def __init__(self, filename, source, description):
+                
+        self.filename = filename
+        self.source = source
+        self.description = description
