525137ec7534c401bef3deaddf99b577e1462170
karussellbremser <kicker789@hotmail.de>
2022-02-04 02:14:25 +0100

    added first version of cover download

--- main.py
+++ main.py
@@ -2,28 +2,31 @@ from media import Media
 from dbcontrol import DBControl
 from scrapelocal import ScrapeLocal
 from scrapeimdboffline import ScrapeIMDbOffline
+from scrapeimdbonline import ScrapeIMDbOnline
 
-
-db = DBControl(':memory:')
-db.createMediaDB()
+#db = DBControl(':memory:')
+#db.createMediaDB()
 
 scrape = ScrapeLocal(r"Y:")
 mediaList = scrape.scrapeLocalComplete()
 #print(db.getAllMedia())
 #print(db.getMediaByYearRange(1992, 1992))
 
-mediaDict = {}
-for x in mediaList:
-    mediaDict[x.imdb_id] = x
+scrapeimdbonline = ScrapeIMDbOnline(r"C:\Users\Sebastian\Desktop\scripting\media-control\covers")
+scrapeimdbonline.downloadCovers(mediaList, 50)
 
-scrapeimdboffline = ScrapeIMDbOffline(r"C:\imdb_datasets")
-ratingDict = scrapeimdboffline.parseIMDbOfflineFile(mediaDict, 0)
-ratingAndBasicsDict = scrapeimdboffline.parseIMDbOfflineFile(ratingDict, 1)
+#mediaDict = {}
+#for x in mediaList:
+#    mediaDict[x.imdb_id] = x
+
+#scrapeimdboffline = ScrapeIMDbOffline(r"C:\imdb_datasets")
+#ratingDict = scrapeimdboffline.parseIMDbOfflineFile(mediaDict, 0)
+#ratingAndBasicsDict = scrapeimdboffline.parseIMDbOfflineFile(ratingDict, 1)
 
 
-db.addMultipleMedia(ratingAndBasicsDict)
+#db.addMultipleMedia(ratingAndBasicsDict)
 
-print(db.getMediaByGenreAND(["Horror", "Comedy"]))
+#print(db.getMediaByGenreAND(["Horror", "Comedy"]))
 
 #print(db.getMediaByRatingRange(80, 100))
 #print(db.getAllMediaSortedByNumVotes())
--- scrapeimdbonline.py
+++ scrapeimdbonline.py
@@ -1,3 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import os.path
+
 class ScrapeIMDbOnline:
+
+    headers = {"Accept-Language": "en-US,en;q=0.5"}
     
+    def __init__(self, cover_directory):
+        self.cover_directory = cover_directory
+    
+    def downloadCovers(self, mediaList, maxCount = 0): # maxCount = 0: unlimited cover downloads
         
+        count = 0
+        
+        for currentMedia in mediaList:
+        
+            # check if file exists, in this case skip this media
+            if os.path.isfile(self.cover_directory + "\\" + currentMedia.getIDString() + ".jpg"):
+                continue
+            
+            # scrape IMDb media main page
+            page = requests.get("https://www.imdb.com/title/" + currentMedia.getIDString() + "/", headers=self.headers)
+            if page.status_code != 200:
+                raise EnvironmentError("no 200 code on page return")
+            soup = BeautifulSoup(page.content, 'html.parser')
+            
+            cover_search_result = soup.find_all(attrs={"aria-label": "View {Title} Poster"})
+            if len(cover_search_result) != 1:
+                raise EnvironmentError("no unique cover tag found")
+            
+            # scrape cover page
+            page = requests.get("https://www.imdb.com" + cover_search_result[0]['href'], headers=self.headers)
+            if page.status_code != 200:
+                raise EnvironmentError("no 200 code on page return")
+            soup = BeautifulSoup(page.content, 'html.parser')
+            
+            cover_search_result = soup.find_all(attrs={"property": "og:image"})
+            if len(cover_search_result) != 1:
+                raise EnvironmentError("no unique cover tag found")
+            
+            cover_direct_link = cover_search_result[0]['content']
+            link_parts = cover_direct_link.rsplit('.', 2)
+            if len(link_parts) != 3 or link_parts[2] != "jpg":
+                raise EnvironmentError("cover link not properly formatted: " + currentMedia.getIDString() + " - " + cover_direct_link)
+            cover_direct_link = link_parts[0] + "._V1_.jpg"
+            
+            # download cover
+            coverFile = requests.get(cover_direct_link, allow_redirects=True)
+            open(self.cover_directory + "\\" + currentMedia.getIDString() + ".jpg", 'wb').write(coverFile.content)
+            
+            count += 1
+            if count == maxCount:
+                return
