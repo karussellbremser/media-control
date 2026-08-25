9784c77cb629f1d31456c0175ab68d2539f97a68
karussellbremser <kicker789@hotmail.de>
2026-08-25 12:15:24 +0200

    removed explicit setting of user agent, as selenium / chrome already does this properly

--- scrapeimdbonline.py
+++ scrapeimdbonline.py
@@ -44,8 +44,6 @@ class ScrapeIMDbOnline:
 
     def __launchBrowser(self, headless):
         chrome_options = Options()
-        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36'
-        chrome_options.add_argument(f'user-agent={user_agent}')
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
         chrome_options.add_experimental_option("useAutomationExtension", False)
