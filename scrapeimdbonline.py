de0df06346f58c28efd5189e5eed877d0a7c7401
karussellbremser <kicker789@hotmail.de>
2026-08-25 11:21:13 +0200

    added some chrome options for consistency

--- scrapeimdbonline.py
+++ scrapeimdbonline.py
@@ -44,8 +44,11 @@ class ScrapeIMDbOnline:
 
     def __launchBrowser(self, headless):
         chrome_options = Options()
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36'
         chrome_options.add_argument(f'user-agent={user_agent}')
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option("useAutomationExtension", False)
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--window-size=1920,1080')
         chrome_options.add_argument("--start-maximized")
